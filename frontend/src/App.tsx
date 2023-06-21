aad32f881a33684a0590130fe588a1af8d370e14
blyal <blyal@protonmail.com>
2023-06-21 15:51:25 +0200

    feat: add Material-UI to frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Button variant='contained' color='primary'>
+      Hello World
+    </Button>
+  );
+}
+
+export { App };
