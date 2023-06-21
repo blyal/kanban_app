1b3d0f733fbf5d1955ba1e9c0a40313c6ef72d11
blyal <blyal@protonmail.com>
2023-06-21 21:50:14 +0200

    add route params and handle 404 route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,19 +23,22 @@ app.post('/boards', (req: Request, res: Response) => {
   res.send('Board posted');
 });
 
-// or patch?
-app.put('/boards', (req: Request, res: Response) => {
-  res.send('Boards updated');
+app.patch('/boards/:id', (req: Request, res: Response) => {
+  res.send(`Board ${req.params.id} updated`);
 });
 
-app.delete('/boards', (req: Request, res: Response) => {
-  res.send('Boards deleted');
+app.delete('/boards/:id', (req: Request, res: Response) => {
+  res.send(`Board ${req.params.id} deleted`);
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send({ message: 'Route not found' });
 });
 
 // error handling
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack); // prints the stack trace to the console
-  res.status(500).send('Something went wrong');
+  res.status(500).send({ message: 'Something went wrong', error: err.message });
 });
 
 // Connect to the database before starting the server
