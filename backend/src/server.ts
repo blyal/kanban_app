26d3118680580ff09fb5810f23d81ae8e4178851
blyal <blyal@protonmail.com>
2023-06-21 17:44:33 +0200

    feat: add basic logging and parsing to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,23 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 5001;
 
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript!');
+});
+
+// error handling
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack); // prints the stack trace to the console
+  res.status(500).send('Something went wrong');
 });
 
 app.listen(port, () => {
