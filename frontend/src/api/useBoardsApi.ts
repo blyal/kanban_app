1cbc68760cb214c9346ccbc0076927240c55bc32
blyal <blyal@protonmail.com>
2023-06-22 15:17:27 +0200

    feat: add type to Boards query

--- frontend/src/api/useBoardsApi.ts
+++ frontend/src/api/useBoardsApi.ts
@@ -1,10 +1,11 @@
 import { useQuery } from 'react-query';
 import { client } from './api-client';
+import { Board } from '../types/types';
 
 const BOARDS_URL = 'boards';
 
 function useGetBoards() {
-  const result = useQuery('boards', () => client(BOARDS_URL));
+  const result = useQuery('boards', () => client<Board[]>(BOARDS_URL));
   return { ...result, boards: result.data?.data ?? [] };
 }
 
