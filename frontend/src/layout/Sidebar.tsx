726ff345174197e25929d26ea3e0a6c2d06f5f20
blyal <blyal@protonmail.com>
2023-06-22 16:02:40 +0200

    feat: map boards into sidebar

--- frontend/src/layout/Sidebar.tsx
+++ frontend/src/layout/Sidebar.tsx
@@ -4,6 +4,9 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Button from '@mui/material/Button';
+import { useBoardsContext } from '../context/boardsContext';
+import { Box, CircularProgress } from '@mui/material';
+import { Board } from '../types/types';
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -11,17 +14,31 @@ interface SidebarProps {
 }
 
 function Sidebar({ isSidebarOpen, closeSidebar }: SidebarProps) {
+  const { boards, isLoading } = useBoardsContext();
+
   return (
     <Drawer anchor='left' open={isSidebarOpen} onClose={closeSidebar}>
       <List>
         {/* Map through boards */}
-        <ListItem>
-          <ListItemText primary='Board 1' />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary='Board 2' />
-        </ListItem>
-        {/* End mapping */}
+        {isLoading ? (
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          boards.map((board: Board) => {
+            return (
+              <ListItem key={board._id}>
+                <ListItemText primary={board.title} />
+              </ListItem>
+            );
+          })
+        )}
         <ListItem>
           <Button color='secondary'>
             <ListItemText primary='Add new board' />
