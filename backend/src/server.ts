179d33cafb1f28faa19092478e8e2caa10b64789
blyal <blyal@protonmail.com>
2023-06-22 13:02:10 +0200

    feat: add get boards functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import { connectDb, closeDb } from './db';
+import Board from './models/board.model';
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -15,8 +16,15 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript!');
 });
 
-app.get('/boards', (req: Request, res: Response) => {
-  res.send('Boards retrieved');
+app.get('/boards', async (req: Request, res: Response) => {
+  try {
+    const boards = await Board.find();
+    console.log(boards);
+    res.send(boards);
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).send({ message: 'Something went wrong' });
+  }
 });
 
 app.post('/boards', (req: Request, res: Response) => {
@@ -36,9 +44,14 @@ app.use((req: Request, res: Response) => {
 });
 
 // error handling
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack); // prints the stack trace to the console
-  res.status(500).send({ message: 'Something went wrong', error: err.message });
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    console.error(err.stack); // prints the stack trace to the console
+    res.status(500).send({ message: 'An error occurred' });
+  } else {
+    console.error(err);
+    res.status(500).send({ message: 'An unknown error occurred' });
+  }
 });
 
 // Connect to the database before starting the server
