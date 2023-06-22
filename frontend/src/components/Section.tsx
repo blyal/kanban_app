dcde81e9dd4241b5443ca5970ecb1d60b69644f7
blyal <blyal@protonmail.com>
2023-06-23 00:25:53 +0200

    feat: add sections to board page

---
+++ frontend/src/components/Section.tsx
@@ -0,0 +1,40 @@
+import { Box, Paper, Typography, List, useTheme } from '@mui/material';
+import { Section as ISection } from '../types/types';
+
+interface SectionProps {
+  section: ISection;
+}
+
+function Section({ section }: SectionProps) {
+  const theme = useTheme();
+  console.log(theme.palette.primary.light);
+  return (
+    <Box
+      sx={{
+        width: 300,
+        mr: 2, // margin right
+        mb: 3, // margin bottom
+        borderRadius: 2,
+        boxShadow: 1,
+        textAlign: 'center',
+      }}
+    >
+      <Paper
+        sx={{
+          backgroundColor: theme.palette.primary.light,
+          padding: 3,
+        }}
+      >
+        <Typography variant='h6'>{section.title}</Typography>
+        <List>
+          {/* {section.tasks.map(task => (
+              <Task key={task.id} task={task} />
+            ))} */}
+          <div>Hello</div>
+        </List>
+      </Paper>
+    </Box>
+  );
+}
+
+export { Section };
--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -1,10 +1,12 @@
-import { Box, CircularProgress, Grid } from '@mui/material';
+import { Box, CircularProgress, Typography, useTheme } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useGetSectionsByBoard } from '../api/useSectionsApi';
 import { ErrorPage } from '../navigation/ErrorPage';
-import { Section } from '../types/types';
+import { Section as ISection } from '../types/types';
+import { Section } from '../components/Section';
 
 function Board() {
+  const theme = useTheme();
   const { boardId } = useParams();
   const { sections, isLoading, isError } = useGetSectionsByBoard(boardId);
 
@@ -29,12 +31,37 @@ function Board() {
   }
 
   return (
-    <Grid container spacing={3}>
-      {sections.map((section: Section) => (
-        // <Section key={section.id} section={section} />
-        <div>{section.title}</div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        overflowX: 'auto',
+        p: 1,
+        margin: 3,
+        alignItems: 'start',
+      }}
+    >
+      {sections.map((section: ISection) => (
+        <Section key={section._id} section={section} />
       ))}
-    </Grid>
+      <Box
+        sx={{
+          width: 300, // whatever fixed width
+          mr: 2,
+          mb: 3,
+          borderRadius: 2,
+          boxShadow: 1,
+          textAlign: 'center',
+          paddingTop: 3,
+          paddingBottom: 3,
+          backgroundColor: theme.palette.primary.main,
+        }}
+      >
+        <Typography variant='h6' color='white'>
+          + Add Section
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
