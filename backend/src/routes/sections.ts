870a7003d79f17a6119ec58b48a8400382dacf05
blyal <blyal@protonmail.com>
2023-06-23 13:42:39 +0200

    feat: implement patch and delete functionality BE

--- backend/src/routes/sections.ts
+++ backend/src/routes/sections.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { v4 as uuid } from 'uuid';
 import Section, { ISection } from '../models/section.model';
+import Task from '../models/task.model';
 
 const router = Router();
 
@@ -34,12 +35,45 @@ router.post('/', async (req: Request, res: Response) => {
   }
 });
 
-router.patch('/:id', (req: Request, res: Response) => {
-  res.send(`Section ${req.params.id} updated`);
+router.patch('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const updatedSection = await Section.findByIdAndUpdate(
+      id,
+      { title },
+      { new: true }
+    );
+    res.send(updatedSection);
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ message: 'Something went wrong with updating a section' });
+  }
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
-  res.send(`Section ${req.params.id} deleted`);
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    // Delete all tasks associated with this section
+    await Task.deleteMany({ sectionId: id });
+
+    // Delete the section
+    const removedSection = await Section.findByIdAndRemove(id);
+    if (!removedSection) {
+      return res.status(404).send({ message: 'Section not found' });
+    }
+
+    res.send({ message: 'Section and associated tasks deleted successfully' });
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ message: 'Something went wrong with deleting a section' });
+  }
 });
 
 export { router };
