967ec9782449d000089f8a58015786ab58b9732f
blyal <blyal@protonmail.com>
2023-06-23 11:33:12 +0200

    feat: add deleteTaskModal

---
+++ frontend/src/modals/DeleteTaskModal.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
+import { useDeleteTask } from '../api/useTasksApi';
+import { Modal } from './Modal';
+import { useModalContext } from '../context/modalContext';
+
+interface DeleteTaskModalProps {
+  taskId: string;
+}
+
+function DeleteTaskModal({ taskId }: DeleteTaskModalProps) {
+  const {
+    mutateAsync: deleteTask,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useDeleteTask();
+  const { closeModal } = useModalContext();
+
+  const handleDelete = () => {
+    deleteTask(taskId);
+  };
+
+  React.useEffect(() => {
+    if (isSuccess) {
+      closeModal();
+    }
+  }, [isSuccess, closeModal]);
+
+  return (
+    <Modal title='Delete Task'>
+      {isLoading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress color='primary' />
+        </Box>
+      ) : isError ? (
+        <div>
+          <p>There was an error deleting a task</p>
+        </div>
+      ) : (
+        <>
+          <Typography>Are you sure you want to delete this task?</Typography>
+          <br />
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            fullWidth
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
+        </>
+      )}
+    </Modal>
+  );
+}
+
+export { DeleteTaskModal };
