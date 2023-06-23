4655940050bc770fd121e4d3d47ebef35344fd24
blyal <blyal@protonmail.com>
2023-06-23 14:45:08 +0200

    feat: insert board title as app header

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,14 +1,43 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useBoardsContext } from '../context/boardsContext';
+import { ModalType, useModalContext } from '../context/modalContext';
 
 interface HeaderProps {
   openSidebar: () => void;
 }
 
 function Header({ openSidebar }: HeaderProps) {
+  const { boards } = useBoardsContext();
+  const { openModal } = useModalContext();
+
+  const [headerTitle, setHeaderTitle] = React.useState<string>('');
+
+  const { pathname } = useLocation();
+  const pathParts = pathname.split('/');
+
+  React.useEffect(() => {
+    if (pathParts[1] === 'boards') {
+      if (pathParts[2]) {
+        // The path is /boards/:boardId
+        const boardId = pathParts[2];
+        const board = boards.find((board) => board._id === boardId);
+        if (board) {
+          setHeaderTitle(board.title);
+        }
+      } else {
+        // The path is /boards
+        setHeaderTitle('All Boards');
+      }
+    }
+  }, [pathParts, boards]);
+
   return (
     <AppBar position='static'>
       <Toolbar className='header-toolbar'>
@@ -22,8 +51,19 @@ function Header({ openSidebar }: HeaderProps) {
           <MenuIcon />
         </IconButton>
         <Typography variant='h6' className='header-title'>
-          Taskz
+          {headerTitle}
         </Typography>
+        {Boolean(pathParts[2]) && (
+          <IconButton
+            edge='end'
+            color='inherit'
+            aria-label='settings'
+            onClick={() => openModal(ModalType.UPDATE_OR_DELETE_BOARD)}
+            style={{ position: 'absolute', right: '10px' }}
+          >
+            <MoreVertIcon />
+          </IconButton>
+        )}
       </Toolbar>
     </AppBar>
   );
