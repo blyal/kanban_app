02d99b6be8c3f80111d29a296797a1b976467ad0
blyal <blyal@protonmail.com>
2023-06-23 11:05:58 +0200

    feat: add delete button

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,4 +1,5 @@
-import { Button, ListItem, Typography, useTheme } from '@mui/material';
+import { Button, Box, ListItem, Typography, useTheme } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { Task as ITask } from '../types/types';
 
 interface TaskProps {
@@ -12,10 +13,12 @@ function Task({ task }: TaskProps) {
     <ListItem
       sx={{
         padding: 0,
+        display: 'flex', // Add this line to use flexbox
+        alignItems: 'center', // This will vertically center your items
       }}
     >
       <Button
-        variant='outlined'
+        // variant='outlined'
         sx={{
           mt: 0.5, // margin top
           mb: 0.5, // margin bottom
@@ -23,11 +26,41 @@ function Task({ task }: TaskProps) {
           width: '100%',
           backgroundColor: theme.palette.primary.main,
           textTransform: 'none',
+          position: 'relative',
         }}
+        onClick={() => console.log('click edit')}
       >
-        <Typography variant='body1' color='white'>
+        <Typography
+          variant='body1'
+          color='white'
+          sx={{ width: '100%', position: 'relative' }}
+        >
           {task.title}
         </Typography>
+        <Box
+          // size='small'
+          // sx={{ ml: 1 }} // You can adjust the margin to your needs
+          onClick={(event) => {
+            event.stopPropagation();
+            console.log('click delete');
+          }} // replace with your delete function
+          sx={{
+            position: 'absolute',
+            mr: 1,
+            right: 0,
+            top: '50%',
+            transform: 'translateY(-50%)',
+          }}
+        >
+          <CloseIcon
+            sx={{
+              position: 'absolute',
+              right: 0,
+              top: '50%',
+              transform: 'translateY(-50%)',
+            }}
+          />
+        </Box>
       </Button>
     </ListItem>
   );
