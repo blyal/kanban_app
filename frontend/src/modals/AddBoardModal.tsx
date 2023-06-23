8ff437c55cdf8e84d2502433c4fd4edee19e8e3a
blyal <blyal@protonmail.com>
2023-06-23 07:33:22 +0200

    feat: add isLoading and isError conditions to AddBoardModal

--- frontend/src/modals/AddBoardModal.tsx
+++ frontend/src/modals/AddBoardModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
+import { Box, Button, CircularProgress, TextField } from '@mui/material';
 import { Modal } from './Modal';
 import { useAddBoard } from '../api/useBoardsApi';
 import { useModalContext } from '../context/modalContext';
@@ -7,7 +7,7 @@ import { useModalContext } from '../context/modalContext';
 function AddBoardModal() {
   const [title, setTitle] = React.useState('');
   const [description, setDescription] = React.useState('');
-  const { mutateAsync: addBoard } = useAddBoard();
+  const { mutateAsync: addBoard, isLoading, isError } = useAddBoard();
   const { closeModal } = useModalContext();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -23,31 +23,47 @@ function AddBoardModal() {
 
   return (
     <Modal title='Add Board'>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label='Title'
-          variant='outlined'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          fullWidth
-          margin='normal'
-          inputProps={{ maxLength: 12 }}
-        />
-        <TextField
-          label='Description'
-          variant='outlined'
-          multiline
-          rows={4}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          fullWidth
-          margin='normal'
-          inputProps={{ maxLength: 140 }}
-        />
-        <Button type='submit' variant='contained' color='primary' fullWidth>
-          Create
-        </Button>
-      </form>
+      {isLoading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress color='primary' />
+        </Box>
+      ) : isError ? (
+        <div>
+          <p>There was an error adding a section</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label='Title'
+            variant='outlined'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+            margin='normal'
+            inputProps={{ maxLength: 12 }}
+          />
+          <TextField
+            label='Description'
+            variant='outlined'
+            multiline
+            rows={4}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            fullWidth
+            margin='normal'
+            inputProps={{ maxLength: 140 }}
+          />
+          <Button type='submit' variant='contained' color='primary' fullWidth>
+            Create
+          </Button>
+        </form>
+      )}
     </Modal>
   );
 }
